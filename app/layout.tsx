9aa7f16ec228e2f6f2fa6b1c335f4a0e6ceaa958
Lucia <39827122+mahdava@users.noreply.github.com>
2025-04-19 00:49:37 +0300

    Add custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Sofia_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const sofiaSans = Sofia_Sans({
+  variable: "--font-sofia-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sofiaSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
