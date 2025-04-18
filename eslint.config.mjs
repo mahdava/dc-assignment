b6c9253436d5d55711afbc693bfb1a5765dd2625
Lucia <39827122+mahdava@users.noreply.github.com>
2025-04-18 23:07:25 +0300

    Initial setup with Next.js

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    // default
+    "next/core-web-vitals",
+    "next/typescript",
+
+    // plugins
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ),
+];
+
+export default eslintConfig;
