dc246346baf6e2388830677502935a71543205c9
Lucia <39827122+mahdava@users.noreply.github.com>
2025-04-22 22:16:29 +0300

    Add eslint rule to prevent use of RAC outside components folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.mjs
 import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -21,9 +22,40 @@ const eslintConfig = [
   ),
   ...compat.config({
     rules: {
+      // We set this off because we need to "duplicate" lot of RAC interfaces
       "@typescript-eslint/no-empty-object-type": "off",
     },
   }),
+
+  ////////////////////////////////////////////////////////////
+  // RAC Components import restriction
+  ////////////////////////////////////////////////////////////
+
+  {
+    // Prevent RAC imports everywhere...
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "react-aria-components",
+          // Ideally here it'd be nice to have more documentation to let teammates know that we want to
+          // restrict this import to the components folder to make sure we don't use unstyled RAC components
+          message:
+            "⚠️ You may only import from 'react-aria-components' inside the `components/` folder",
+        },
+      ],
+    },
+  },
+
+  // ...except for components/**/*.{js,jsx,ts,tsx}
+  {
+    files: ["components/**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "no-restricted-imports": "off",
+    },
+  },
+
+  ////////////////////////////////////////////////////////////
 ];
 
 export default eslintConfig;
